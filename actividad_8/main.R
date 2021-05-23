1b819c025e25d1ce1ce82fdccdc0d0770173d388
Salvador <55848411+Salvador243@users.noreply.github.com>
2021-05-23 18:15:29 -0500

    Add files via upload

---
+++ actividad_8/main.R
@@ -0,0 +1,26 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: salva
+# Created on: 17/05/2021
+
+# EJERCICIOS DE LA ACTIVIDAD 8
+
+answer <- 0;
+for (j in 3:5){
+  answer <- j + answer
+}
+paste('Ejercicio 1: ',answer)
+
+
+answer <- 10;
+for (j in 3:5){
+  answer <- j + answer
+}
+paste('Ejercicio 2: ',answer)
+
+
+answer <- 10;
+for (j in 3:5){
+  answer <- j * answer
+}
+paste('Ejercicio 3: ',answer)
